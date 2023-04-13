6437c2066f947d6ce1fdd21764ecdb652f0ff71a
ArduinoNa ขายอุปกรณ์ Arduino, เครื่องมือช่าง, raspberry pi, สอน ROS <thanabadee_bc@hotmail.com>
2023-04-13 12:29:11 +0700

    Merging to Arduino Libraries Manager

--- src/ModuleMoreSumoV2.h
+++ src/ModuleMoreSumoV2.h
@@ -1,7 +1,6 @@
 #include "enemy_detector.h"
 #include "line_detector.h"
-#include "driver.h"
-#include "statemachines/statemachine.h"
+#include "driver.h" 
 #include "robot_ui.h"
 #include "helper_def.h"
 using namespace mdmbot;
